85760877e167f7584446d6b0ff706eb8fcd69091
Eli Cusic <tntitans31@gmail.com>
2021-04-03 08:58:12 -0500

    set up check API function with button

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import React, { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
 
+  const checkAPI = () => {
+    fetch("https://my-sous-backend.herokuapp.com/things")
+    .then(resp =>  resp.json())
+    .then(things => console.log(things))
+  }
+
   return (
     <div>
       <h1>The count was: { count }</h1> 
       <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
+      <button onClick={ checkAPI }>Check API</button>
     </div>
   );
 }
